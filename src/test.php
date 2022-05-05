ca84ffc8cbf7bab17c1141ded9b886f8e3382a02
shvarev <v.m.shvarev@yandex.ru>
2022-05-05 23:04:10 +0300

    Add client code in test.php file

--- src/test.php
+++ src/test.php
@@ -3,8 +3,20 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Domain\Booking\FilmShow\FilmShow;
+use App\Domain\Booking\Ticket\ValueObject\Client;
 use App\Domain\Booking\Ticket\ValueObject\Film;
 
-$film = new Film('Batman');
-$filmShow1 = new FilmShow($film, 15, 14);
-$filmShow1->createTickers(3);
+$film = new Film('Batman', 173);
+
+$filmShow1 = new FilmShow(1, $film);
+
+$filmShow1->setDateAndTime('2022-05-10 20:00');
+$filmShow1->createTickers(10);
+
+$client1 = new Client('Vitaliy', '89082222222');
+$client2 = new Client('Nastya', '89087777777');
+
+$filmShow1->bookTicket($client1);
+$filmShow1->bookTicket($client2);
+
+print_r($filmShow1->getInfoAboutFilmShow());
