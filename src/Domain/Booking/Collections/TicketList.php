47529a4f9f2bb0c8308f4c14ea4ed49c55a1e652
shvarev <v.m.shvarev@yandex.ru>
2022-05-06 11:49:32 +0300

    FIX-8 Fix DateTime import

--- src/Domain/Booking/Collections/TicketList.php
+++ src/Domain/Booking/Collections/TicketList.php
@@ -4,6 +4,7 @@ namespace App\Domain\Booking\Collections;
 
 use App\Domain\Booking\Entities\Film;
 use App\Domain\Booking\Entities\Ticket;
+use DateTime;
 use Iterator;
 
 final class TicketList implements Iterator
@@ -13,7 +14,7 @@ final class TicketList implements Iterator
 
     private int $pointer = 0;
 
-    public function createTicketList(int $countOfTickets, Film $film, \DateTime $dateTimeStart, \DateTime $dateTimeEnd): void
+    public function createTicketList(int $countOfTickets, Film $film, DateTime $dateTimeStart, DateTime $dateTimeEnd): void
     {
         for ($i = 0; $i < $countOfTickets; $i++) {
             $this->tickets[] = new Ticket($film, $dateTimeStart, $dateTimeEnd);
